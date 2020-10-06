ed72061a3595fc5d84e9c831e6a179dbcce30053
moonlaughs <iza8848@gmail.com>
2020-10-06 12:09:26 +0200

    first commit

---
+++ CommandHistory/CommandHistory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandHistory
+{
+    public class CommandHistory
+    {
+        private static  CommandHistory instance;
+
+        private List<string> MyCommandHistoryList { get; set; }
+
+        private CommandHistory()
+        {
+            MyCommandHistoryList = new List<string>();
+        }
+
+        public static CommandHistory getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CommandHistory();
+            }
+            return instance;
+        }
+
+        public void AddToList(string input)
+        {
+            MyCommandHistoryList.Add(input);
+        }
+
+        public void RemoveFromList(string input)
+        {
+            MyCommandHistoryList.Remove(input);
+        }
+    }
+}
---
+++ CommandHistory/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandHistory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CommandHistory obj = CommandHistory.getInstance();
+
+            string myInput = Console.ReadLine();
+            obj.AddToList(myInput);
+            string myInput1 = Console.ReadLine();
+            obj.AddToList(myInput1);
+
+            //Console.WriteLine("=============added");
+
+            //foreach (var item in obj.MyCommandHistoryList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            string myInput2 = Console.ReadLine();
+            obj.RemoveFromList(myInput2);
+
+            //Console.WriteLine("=================removed");
+            //foreach (var item in obj.MyCommandHistoryList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //bool _continue = true;
+            //while (_continue == true)
+            //{
+            //    string myInput = Console.ReadLine();
+            //    //obj.MyCommandHistoryList.Add(myInput);
+            //    Console.WriteLine("Wanna continue?");
+            //    string _answer = Console.ReadLine();
+            //    //obj.MyCommandHistoryList.Add(_answer);
+            //    if (_answer == "no")
+            //        _continue = false;
+            //}
+
+            //foreach (var item in obj.MyCommandHistoryList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+        }
+    }
+}
